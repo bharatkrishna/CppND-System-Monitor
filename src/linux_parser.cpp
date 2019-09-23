8e2da41bbb08348105f981ad897f102feaca7cca
Bharat Krishna <bharatkrishna.r@gmail.com>
2019-09-22 17:14:20 -0700

    Finishing up basic system info

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -93,7 +93,17 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  string line;
+  long uptime;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> uptime;
+  }
+  return uptime;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
